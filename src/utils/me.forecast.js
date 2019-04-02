33af6e8fa6db7ac700d531066b2114c3734551a5
melodycastromero <melodycastromero@aol.com>
2019-04-02 16:37:02 +0800

    Adding temp high and low to forecast

--- src/utils/me.forecast.js
+++ src/utils/me.forecast.js
@@ -10,7 +10,10 @@ const url = 'https://api.darksky.net/forecast/46c9392f45cb8ee69a2bf2dd27c27c48/'
         } else {
             callback( undefined, body.daily.data[0].summary + ' It is currently ' +  
             body.currently.temperature  + " degrees out. There is a " + 
-            body.currently.precipProbability + "% chance of rain.")
+            body.currently.precipProbability + "% chance of rain. With the temperature high of " + 
+            body.daily.data[0].temperatureHigh + " with the low of " +
+            body.daily.data[1].temperatureLow 
+            )
         }
     })
 }
